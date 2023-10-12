8adaf7cfd0562f764aa2fea50c5a45c77e8c63fb
EgorVolobuevv <144919257+EgorVolobuevv@users.noreply.github.com>
2023-10-12 21:23:08 +0300

    Add files via upload

--- Работа 1 Волобуев/Работа 1 Волобуев/Program.cs
+++ Работа 1 Волобуев/Работа 1 Волобуев/Program.cs
@@ -1,169 +1,98 @@
-﻿
-using System;
+﻿using System;
 class Program
 {
     static void Main(string[] args)
     {
-        int a;
-        int b;
-        double c;
-        decimal d;
+        double xAbs, xACosSinTan, xAtan21, xAtan22, xCosSinTan, xCoshSinhTanh, xEquals1, xEquals2, xExp, xLog1, xLog2, xLog10, xMaxMin1, xMaxMin2, xPow1, xPow2, xRound, xSign, xSqrt;
+        int xBigMul1, xBigMul2;
+        decimal xCeiling, xFloor, xTruncate;
+
 
         Console.WriteLine("Нажмите на любую кнопку, чтобы начать общую часть работы");
-
         Console.ReadKey(true);
+        Console.WriteLine("-------------------------------------------------------------------------------------------------");
 
         Console.WriteLine("Введите число, для получения его модуля");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Модуль данного числа равен" + " " + Math.Abs(a));
-
+        xAbs = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите число от -1 до 1, для получения тригонометрических функций");
-        c = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Угол, косинус которого равен указанному числу равен" + " " + Math.Acos(c));
-        Console.WriteLine("Угол, синус которого равен указанному числу равен" + " " + Math.Asin(c));
-        Console.WriteLine("Угол, тангенс которого равен указанному числу равен " + " " + Math.Atan(c));
-
+        xACosSinTan = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Введите число, чтобы получить угол, тангенс которого равен отношению двух указанных чисел");
-        double x;
-        double y;
         Console.WriteLine("Введите занчение x и y:");
-        x = Convert.ToDouble(Console.ReadLine());
-        y = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Угол, тангенс которого равен отношению двух указанных чисел:" + " " + Math.Atan2(x, y));
-
+        xAtan21 = Convert.ToDouble(Console.ReadLine());
+        xAtan22 = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Введите два целых числа для их произведения:");
-        a = Convert.ToInt32(Console.ReadLine());
-        b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Произведение данных чисел равно" + " " + Math.BigMul(a, b));
-
+        xBigMul1 = Convert.ToInt32(Console.ReadLine());
+        xBigMul2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите число, чтобы получить наименьшее целое число, которое больше или равно указанному числу");
-        d = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine(Math.Ceiling(d));
-
+        xCeiling = Convert.ToDecimal(Console.ReadLine());
         Console.WriteLine("Введите угол, измеряемый в радианах, для получение его косинуса, синуса и тангенса");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("косинус:" + " " + Math.Cos(a));
-        Console.WriteLine("синус:" + " " + Math.Sin(a));
-        Console.WriteLine("тангенс" + " " + Math.Tan(a));
-
+        xCosSinTan = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите угол, измеряемый в радианах, для получения гиперболического косинуса, синуса и тангенса");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("гиперболический косинус:" + " " + Math.Cosh(a));
-        Console.WriteLine("гиперболический синус:" + " " + Math.Sinh(a));
-        Console.WriteLine("гиперболический тангенс:" + " " + Math.Tanh(a));
-
+        xCoshSinhTanh = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Нажмите любую клавишу, для получения математических констант");
         Console.ReadKey(true);
+        Console.WriteLine("Введите два числа, чтобы проверить их равность");
+        xEquals1 = Convert.ToInt32(Console.ReadLine());
+        xEquals2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число, для возведения числа 'e' в указанную степень");
+        xExp = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число, чтобы получить наибольшее целое число, которое меньше или равно указанному числу");
+        xFloor = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Введите аргумент и основание для логарифма, чтобы получить ответ на логарифм");
+        xLog1 = Convert.ToInt32(Console.ReadLine());
+        xLog2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите аргумент, для десятичного логарифма, чтобы получить ответ на десятичный логарифм");
+        xLog10 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите два числа, чтобы получить максимальное и минимальное из них");
+        xMaxMin1 = Convert.ToInt32(Console.ReadLine());
+        xMaxMin2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число и степень, в которую его нужно возвести");
+        xPow1 = Convert.ToInt32(Console.ReadLine());
+        xPow2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число, для его округления");
+        xRound = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите число. Если оно отрицательное, то программа вернет '-1', если положительное, то '1', если число равно 0, '0'");
+        xSign = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число, чтобы узнать его корень");
+        xSqrt = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите десятичное число, чтобы получить его целую часть");
+        xTruncate = Convert.ToDecimal(Console.ReadLine());
         Console.WriteLine("-------------------------------------------------------------------------------------------------");
+        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
+        Console.ReadKey(true);
+        Console.WriteLine("-------------------------------------------------------------------------------------------------");
+
+        Console.WriteLine("Модуль данного числа равен" + " " + Math.Abs(xAbs));
+        Console.WriteLine("Угол, косинус которого равен указанному числу равен" + " " + Math.Acos(xACosSinTan));
+        Console.WriteLine("Угол, синус которого равен указанному числу равен" + " " + Math.Asin(xACosSinTan));
+        Console.WriteLine("Угол, тангенс которого равен указанному числу равен " + " " + Math.Atan(xACosSinTan));
+        Console.WriteLine("Угол, тангенс которого равен отношению двух указанных чисел:" + " " + Math.Atan2(xAtan21, xAtan22));
+        Console.WriteLine("Произведение данных чисел равно" + " " + Math.BigMul(xBigMul1, xBigMul2));
+        Console.WriteLine("Наименьшее целое число, которое больше или равно указанному числу:" + "" + Math.Ceiling(xCeiling));
+        Console.WriteLine("косинус:" + " " + Math.Cos(xCosSinTan));
+        Console.WriteLine("синус:" + " " + Math.Sin(xCosSinTan));
+        Console.WriteLine("тангенс" + " " + Math.Tan(xCosSinTan));
+        Console.WriteLine("гиперболический косинус:" + " " + Math.Cosh(xCoshSinhTanh));
+        Console.WriteLine("гиперболический синус:" + " " + Math.Sinh(xCoshSinhTanh));
+        Console.WriteLine("гиперболический тангенс:" + " " + Math.Tanh(xCoshSinhTanh));
         Console.WriteLine("Экспонента:" + " " + Math.E);
         Console.WriteLine("Число ПИ:" + " " + Math.PI);
-
-        Console.WriteLine("Введите два числа, чтобы проверить их равность");
-        a = Convert.ToInt32(Console.ReadLine());
-        b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
+        Console.WriteLine("Равны?" + " " + Math.Equals(xEquals1, xEquals2));
+        Console.WriteLine("Результат возведения числа 'e' в эту степень:" + " " + Math.Exp(xExp));
+        Console.WriteLine("Наибольшее целое число, которое меньше или равно указанному числу:" + " " + Math.Floor(xFloor));
+        Console.WriteLine("Ответ на логарифм:" + " " + Math.Log(xLog1, xLog2));
+        Console.WriteLine("Ответ на логарифм:" + " " + Math.Log10(xLog10));
+        Console.WriteLine("максимальное число:" + " " + Math.Max(xMaxMin1, xMaxMin2));
+        Console.WriteLine("минимальное число:" + " " + Math.Min(xMaxMin1, xMaxMin2));
+        Console.WriteLine("Число в степени:" + " " + Math.Pow(xPow1, xPow2));
+        Console.WriteLine("Округленное число:" + " " + Math.Round(xRound));
+        Console.WriteLine(Math.Sign(xSign));
+        Console.WriteLine("Корень этого числа:" + " " + Math.Sqrt(xSqrt));
+        Console.WriteLine("Целая часть этого числа:" + " " + Math.Truncate(xTruncate));
         Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine(Math.Equals(a, b));
-
-        Console.WriteLine("Введите число, для возведения числа 'e' в указанную степень");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Результат возведения числа 'e' в эту степень:" + " " + Math.Exp(a));
-
-        Console.WriteLine("Введите число, чтобы получить наибольшее целое число, которое меньше или равно указанному числу");
-        d = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Наибольшее целое число, которое меньше или равно указанному числу:" + " " + Math.Floor(d));
-
-        Console.WriteLine("Введите аргумент и основание для логарифма, чтобы получить ответ на логарифм");
-        a = Convert.ToInt32(Console.ReadLine());
-        b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Ответ на логарифм:" + " " + Math.Log(a, b));
-
-        Console.WriteLine("Введите аргумент, для десятичного логарифма, чтобы получить ответ на десятичный логарифм");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Ответ на логарифм:" + " " + Math.Log10(a));
-
-        Console.WriteLine("Введите два числа, чтобы получить максимальное и минимальное из них");
-        a = Convert.ToInt32(Console.ReadLine());
-        b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("максимальное число:" + " " + Math.Max(a, b));
-        Console.WriteLine("минимальное число:" + " " + Math.Min(a, b));
-
-        Console.WriteLine("Введите число и степень, в которую его нужно возвести");
-        a = Convert.ToInt32(Console.ReadLine());
-        b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Число в степени:" + " " + Math.Pow(a, b));
-
-        Console.WriteLine("Введите число, для его округления");
-        c = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Округленное число:" + " " + Math.Round(c));
-
-        Console.WriteLine("Введите число. Если оно отрицательное, то программа вернет '-1', если положительное, то '1', если число равно 0, '0'");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine(Math.Sign(a));
-
-        Console.WriteLine("Введите число, чтобы узнать его корень");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Корень этого числа:" + " " + Math.Sqrt(a));
-
-        Console.WriteLine("Введите десятичное число, чтобы получить его целую часть");
-        d = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine("Нажмите любую кнопку, для начала расчетов");
-        Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
-        Console.WriteLine("Целая часть этого числа:" + " " + Math.Truncate(d));
 
         Console.WriteLine("Нажмите любую кнопку, чтобы начать вторую часть работы");
         Console.ReadKey(true);
-        Console.WriteLine("-------------------------------------------------------------------------------------------------");
         Console.WriteLine("Вариант 13");
         double left = Math.Round(Math.Acos(1 / 2.0) * (180.0 / Math.PI));
         double right = 60.0;
